64aa46d2f1f09a8118c7f26b450a6fa903f74a34
labu28 <rayhanlabib7@gmail.com>
2024-02-17 16:45:38 +0600

    add Multifly.cpp

---
+++ Multiply.cpp
@@ -0,0 +1,57 @@
+                                           
+
+#include <bits/stdc++.h>
+
+#define     ll                                 long long int   
+
+#define     ll_in1(a)                          cin>>a
+
+#define     ll_in2(a,b)                        cin>>a>>b
+
+#define     ll_in3(a,b,c)                      cin>>a>>b>>c
+
+#define     take_vector_int(x,o,n)             for(int i=o;i<=n;i++){cin>>x[i];}    
+
+#define     srt(v)                             sort(v.begin(),v.end());
+
+#define     rsrt(v)                            reverse(v.begin(),v.end())
+
+#define     gcd(a, b)                          __gcd(a, b)
+
+#define     vi                                 vector<int>
+
+#define     vll                                vector<ll>
+
+#define     endl                               '\n'
+
+const int dx[] = {+1,-1,+0,+0}; ///Rock's Move
+
+const int dy[] = {+0,+0,+1,-1}; ///Rock's Move
+
+
+const       ll                                 INF = 1122334455667788990;
+#define all(a) a.begin(),a.end()
+
+#define Unique(x)                             (x).erase(unique(all(x)),(x).end())
+
+#define     yes                                cout<<"YES"<<endl
+
+#define     no                                 cout<<"NO"<<endl
+
+#define     mem(a,i)                           memset(a, i, sizeof(a))
+
+#define     Labib                              ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define mod   1000003                 
+using namespace std;
+
+
+
+
+int main()
+{
+  int a=4;
+  int b=2;
+  cout<<(a*b)<<endl;
+
+    return 0;
+}
